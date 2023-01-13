7956cb865be4a49590e083d40e0bdbd485c27e24
sdidd <77382851+sdidd@users.noreply.github.com>
2023-01-13 10:45:27 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Note from './components/Note.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        	<h2 className = "App-header-text">NoterLITE<img src={logo} className="App-logo" alt="logo" /></h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <div>
+        	<Note/>
+        </div>
       </div>
     );
   }
