cb6bb0c6362108be04b21d455c10d8f19cdfd5ea
sdidd <77382851+sdidd@users.noreply.github.com>
2023-10-17 13:26:19 +0000

    Ignoired not required files

--- server/src/mongo.js
+++ server/src/mongo.js
@@ -1,31 +1,31 @@
-const username = encodeURIComponent("sdidd");
-const password = encodeURIComponent("5gy32&@04");
+const username = encodeURIComponent("user");
+const password = encodeURIComponent("user");
 
 
 
-  const { MongoClient, ServerApiVersion } = require('mongodb');
-  const uri = "mongodb+srv://${username}:${password}@buildex.t5zyfgm.mongodb.net/?retryWrites=true&w=majority";
-  
-  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://user:user@buildex.t5zyfgm.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-  run().catch(console.dir);
-  
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
